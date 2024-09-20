a0b1bd3c24d26246142e72f11603720f34a92e87
Opistorh <artem199716@gmail.com>
2024-09-20 21:27:54 +0500

    хуй

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Alert } from "@lemonsqueezy/wedges";
+import { Button } from "@lemonsqueezy/wedges";
 
 function App() {
   return (
     <div className="p-4">
       <Alert>You're awesome!</Alert>
+      <Button>Button</Button>
     </div>
   );
 }
